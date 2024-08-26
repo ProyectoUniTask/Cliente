d4dd1745cbaad2269f64c01705c6dccab967f3a0
VillalbaRocio <144244898+VillalbaRocio@users.noreply.github.com>
2024-08-25 23:51:09 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/EventsCalendar'; 
-import SubjectList from './components/SubjectList';
-import SubjectForm from './components/SubjectForm';
-import SubjectEdit from './components/SubjectEdit';
+import Register from './components/Register'; 
+import CourseList from './components/CourseList';
+import CourseForm from './components/CourseForm';
+import CourseEdit from './components/CourseEdit';
 import ExamList from './components/ExamList';
 import ExamForm from './components/ExamForm';
 import ExamEdit from './components/ExamEdit';
+import SubjectList from './components/SubjectList';
+import SubjectForm from './components/SubjectForm';
+import SubjectEdit from './components/SubjectEdit';
+import CalendarPage from './components/CalendarPage';
 
 import './App.css';
 function App() {
@@ -18,14 +21,16 @@ function App() {
       <Routes>
         <Route path="/" element = {<Login/>}/>
         <Route path="/register" element = {<Register/>}/>
-        <Route path="/home" element = {<Home/>}/>
-        <Route path="/home" element={<MainComponent />} />
-        <Route path="/subject" element = {<SubjectList/>}/>
-        <Route path="/subject/new" element = {<SubjectForm/>}/>
-        <Route path="/subject/:id/edit" element = {<SubjectEdit/>}/>
+        <Route path="/home" element={<CalendarPage />} />
+        <Route path="/course" element = {<CourseList/>}/>
+        <Route path="/course/new" element = {<CourseForm/>}/>
+        <Route path="/course/:id/edit" element = {<CourseEdit/>}/>
         <Route path="/exam" element = {<ExamList/>}/>
         <Route path="/exam/new" element = {<ExamForm/>}/>
         <Route path="/exam/:id/edit" element = {<ExamEdit/>}/>
+        <Route path="/subject" element = {<SubjectList/>}/>
+        <Route path="/subject/new" element = {<SubjectForm/>}/>
+        <Route path="/subject/:id/edit" element = {<SubjectEdit/>}/>
       </Routes>
     </BrowserRouter>
     </div>
